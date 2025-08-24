1e84d01230f0b410d93bc0259d7f0814402eaa8a
siva5683 <mr.joker87654@gmail.com>
2025-08-24 14:32:16 +0530

    chg

--- javascript/JS.js
+++ javascript/JS.js
@@ -1,35 +1,21 @@
+
 var countDown = document.getElementById('countDown');
 var bgImage = document.getElementById('bg-image');
-var initialCountdown = parseInt(countDown.innerHTML);
 
-// Set initial background image on load
-if (initialCountdown % 2 === 0) {
-  bgImage.src = "images/bg2.jpg";  // Even image
-} else {
-  bgImage.src = "images/bg1.jpg";  // Odd image
-}
+var initialCountdown = countDown.innerHTML;
 
 var interval = setInterval(function () {
-  if (initialCountdown <= 0) {
-    clearInterval(interval);
-    return;
-  }
+    initialCountdown = initialCountdown - 1;
+    countDown.innerHTML = initialCountdown;
 
-  initialCountdown = initialCountdown - 1;
-  countDown.innerHTML = initialCountdown;
 
-  // Decrease font size dynamically
-  countDown.style.fontSize = initialCountdown * 100 + "px";
+    countDown.style.fontSize = initialCountdown * 100 + "px";
 
-  // Scale background with respect to current countdown number
-  let scaleValue = initialCountdown / 10; // 10 → 1, 0 → 0
-  if (scaleValue < 0) scaleValue = 0;
-  bgImage.style.transform = "scale(" + scaleValue + ")";
+    var imagePath = initialCountdown % 2 === 0 ? './images/bg1.jpg' : './images/bg2.jpg';
+    bgImage.src = imagePath;
+    bgImage.style.width = initialCountdown * 100 + "px";
 
-  // Change background image for even and odd numbers
-  if (initialCountdown % 2 === 0) {
-    bgImage.src = "images/bg2.jpg";
-  } else {
-    bgImage.src = "images/bg1.jpg";
-  }
+    if (initialCountdown <= 0) {
+        clearInterval(interval);
+    }
 }, 1000);
