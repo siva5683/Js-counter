4061dd2ea3d8ba2b82d403af8cc4bf053d45ad4e
siva5683 <mr.joker87654@gmail.com>
2025-08-24 14:37:44 +0530

    change

--- javascript/JS.js
+++ javascript/JS.js
@@ -2,7 +2,7 @@
 var countDown = document.getElementById('countDown');
 var bgImage = document.getElementById('bg-image');
 
-<<<<<<< HEAD
+
 var initialCountdown = countDown.innerHTML;
 
 var interval = setInterval(function () {
@@ -20,28 +20,5 @@ var interval = setInterval(function () {
         clearInterval(interval);
     }
 }, 1000);
-=======
 
 
-var initialCountdown = countDown.innerHTML;
-
-
-var interval = setInterval(function () {
-
-    initialCountdown = initialCountdown - 1;
-
-    countDown.innerHTML = initialCountdown
-
-    countDown.style.fontSize = initialCountdown * 100 + "px";
-
-
-    var imagePath = initialCountdown % 2 === 0 ? './images/bg1.jpg' : './images/bg2.jpg';
-    bgImage.src = imagePath;
-    console.log(bgImage.src);
-
-    if (initialCountdown <= 0) {
-        clearInterval(interval)
-    }
-
-}, 1000)
->>>>>>> 1e5c8106ee9f3de0904bb0939cecdd571cc97b70
