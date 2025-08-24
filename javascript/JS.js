9ed09198e3f6013dece38e5b448296c2fd3c05f0
siva5683 <mr.joker87654@gmail.com>
2025-08-24 13:54:59 +0530

    Update JS.js

--- javascript/JS.js
+++ javascript/JS.js
@@ -1,35 +1,38 @@
 var countDown = document.getElementById('countDown');
 var bgImage = document.getElementById('bg-image');
-var initialCountdown = parseInt(countDown.innerHTML);
 
-// Set initial background image on load
-if (initialCountdown % 2 === 0) {
-  bgImage.src = "images/bg2.jpg";  // Even image
-} else {
-  bgImage.src = "images/bg1.jpg";  // Odd image
-}
+// console.log(countDown);
+// console.log(bgImage);
+
+var initialCountdown = countDown.innerHTML;
+// console.log(initialCountdown);
+
+// console.log(bgImage.src);
+// bgImage.src = './images/bg2.jpg';
 
 var interval = setInterval(function () {
-  if (initialCountdown <= 0) {
-    clearInterval(interval);
-    return;
-  }
 
-  initialCountdown = initialCountdown - 1;
-  countDown.innerHTML = initialCountdown;
+    initialCountdown = initialCountdown - 1;
+    // console.log(initialCountdown);
+    countDown.innerHTML = initialCountdown
 
-  // Decrease font size dynamically
-  countDown.style.fontSize = initialCountdown * 100 + "px";
+    // console.log(initialCountdown * 100 + "px");
+    countDown.style.fontSize = initialCountdown * 100 + "px";
 
-  // Scale background with respect to current countdown number
-  let scaleValue = initialCountdown / 10; // 10 → 1, 0 → 0
-  if (scaleValue < 0) scaleValue = 0;
-  bgImage.style.transform = "scale(" + scaleValue + ")";
+    // if (initialCountdown % 2 === 0) {
+    //     bgImage.src = './images/bg1.jpg';
+    // }
+    // else {
+    //     bgImage.src = './images/bg2.jpg';
+    // }
+    // console.log(bgImage.src);
 
-  // Change background image for even and odd numbers
-  if (initialCountdown % 2 === 0) {
-    bgImage.src = "images/bg2.jpg";
-  } else {
-    bgImage.src = "images/bg1.jpg";
-  }
-}, 1000);
+    var imagePath = initialCountdown % 2 === 0 ? './images/bg1.jpg' : './images/bg2.jpg';
+    bgImage.src = imagePath;
+    console.log(bgImage.src);
+
+    if (initialCountdown <= 0) {
+        clearInterval(interval)
+    }
+
+}, 1000)
